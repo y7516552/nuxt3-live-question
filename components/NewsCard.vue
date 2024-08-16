afce5a6a7f77c0c0f5e622adc05b2ffd38e77bcd
JasonLu <s099113302@gmail.com>
2024-08-16 14:41:20 +0800

    uplaod question

---
+++ components/NewsCard.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+defineProps({
+    _id: String,
+    title: String,
+    image: String,
+    description: String,
+    createdAt: String,
+    updatedAt: String,
+});
+</script>
+
+<template>
+    <div class="card">
+        <div class="card-image">
+            <img :src="image" :alt="title" />
+        </div>
+        <div class="card-body">
+            <h3 class="card-title">{{ title }}</h3>
+            <p class="card-description">
+                {{ description }}
+            </p>
+            <div class="card-timestamps">
+                <p><strong>Created At:</strong> {{ createdAt?.split("T")[0] }}</p>
+                <p><strong>Updated At:</strong> {{ updatedAt?.split("T")[0] }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style src="@/assets/stylesheets/components/card.scss"></style>
---
+++ pages/index.vue
@@ -0,0 +1,36 @@
+<script setup>
+import Loading from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/css/index.css';
+
+const newsList = ref([]);
+const isLoading = ref(false);
+
+// API 路徑 : https://nuxr3.zeabur.app/api/v1/home/news/
+// 使用 ES6 fetch() 或是 axios.get() 串接 API
+// 切換 isLoading 狀態
+
+
+
+</script>
+
+<template>
+
+  <div class="container">
+    <h1>
+      最新消息
+    </h1>
+    <!-- <NewsCard v-for="..." :key="..."  v-bind="..." /> -->
+
+    <ClientOnly>
+      <Loading v-model:active="isLoading" />
+    </ClientOnly>
+  </div>
+</template>
+
+
+<style scoped>
+.container {
+  max-width: 1000px;
+  margin: 0 auto;
+}
+</style>
