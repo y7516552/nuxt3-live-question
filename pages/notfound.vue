a7c01a0b459cbb3b759b71ee8402d42064a0474c
JasonLu <s099113302@gmail.com>
2024-08-26 22:39:24 +0800

    調整 notfound.vue 樣式

--- pages/notfound.vue
+++ pages/notfound.vue
@@ -10,8 +10,6 @@ const page = "xxx";
   </div>
 </template>
 
-<script setup></script>
-
 <style scoped>
 .not-found {
   display: flex;
@@ -19,7 +17,6 @@ const page = "xxx";
   align-items: center;
   justify-content: center;
   height: 100vh;
-  text-align: center;
 }
 
 .not-found h1 {
@@ -30,20 +27,5 @@ const page = "xxx";
 .not-found p {
   font-size: 1.5rem;
   margin-bottom: 2rem;
-}
-
-.not-found a {
-  font-size: 1.2rem;
-  color: #007bff;
-  text-decoration: none;
-  border: 2px solid #007bff;
-  padding: 0.5rem 1rem;
-  border-radius: 5px;
-  transition: background-color 0.3s, color 0.3s;
-}
-
-.not-found a:hover {
-  background-color: #007bff;
-  color: #fff;
 }
 </style>
