e4841baeefcf0753da4fac4fcfa3f1c74f6d3406
JasonLu <s099113302@gmail.com>
2024-08-28 22:37:28 +0800

    upload question

---
+++ README.md
@@ -0,0 +1,65 @@
+# Day 9 - $fetch 與 ofetch 題目
+
+![題目示意圖](image.png)
+
+請 clone 這一份模板，實作帳號註冊功能 :
+
+- 在 `/pages/register.vue` 使用模板提供的操作介面填寫註冊表單。點擊 “註冊” 按鈕後使用 Nuxt3 提供的方法串接旅館的 [註冊 API](https://nuxr3.zeabur.app/api/v1/user/signup) ，將請求送出。
+- 需使用 try catch 處理請求成功與失敗的訊息，請求成功與失敗皆使用 [sweetAlert2 套件](https://sweetalert2.github.io/)顯示訊息。sweetAlert2 套件在模板已有安裝與引入，不需再額外設定。
+
+```jsx
+$swal.fire({
+  position: "center",
+  icon: ... ,
+  title: ... ,
+  showConfirmButton: false,
+  timer: 1500,
+});
+```
+
+- 表單不需處理表單驗證、身分驗證、檢查登入狀態以及存入 cookie。
+- [註冊 API](https://nuxr3.zeabur.app/api/v1/user/signup) 夾帶的請求體（Request Body）格式，需要注意以下地方 :
+  - 所有欄位都必填。
+  - 密碼需要至少 8 碼以上，並英數混合。
+  - 電話格式可以是手機號碼與市內電話。
+  - birthday 格式可以是 "yyyy-mm-dd”。
+  - zipcode 需要對照到各縣市各區的郵遞區號，可以參考 [郵遞區號速查一覽表](https://c2e.ezbox.idv.tw/zipcode.php)。
+- 串接 API 時需避免在伺服器端與客戶端重複發送請求。
+
+## 安裝
+
+以下將會引導你如何安裝此專案到你的電腦上。
+
+Node.js 版本建議為：`18.0.0` 以上
+
+### 取得專案
+
+```bash
+git clone -b day9-nuxt3-fetch  https://github.com/jasonlu0525/nuxt3-live-question.git day9-nuxt3-fetch-question
+```
+
+### 移動到專案內
+
+```bash
+cd  day9-nuxt3-fetch-question
+```
+
+### 安裝套件
+
+```bash
+npm install
+```
+
+### 運行專案
+
+```bash
+npm run dev
+```
+
+### 開啟專案
+
+在瀏覽器網址列輸入以下即可看到畫面
+
+```bash
+http://localhost:3000/
+```
---
+++ pages/register.vue
@@ -0,0 +1,136 @@
+<script setup>
+const { $swal } = useNuxtApp();
+
+// 表單格式
+const userRegisteObject = ref({
+  name: "",
+  email: "",
+  password: "",
+  phone: "",
+  birthday: "",
+  address: {
+    zipcode: "",
+    detail: "",
+  },
+});
+// 使用 sweetAlert2 套件顯示訊息
+// $swal.fire({
+//   position: "center",
+//   icon: ...,
+//   title: ...,
+//   showConfirmButton: false,
+//   timer: 1500,
+// });
+</script>
+
+<template>
+  <div class="bg-light py-3 py-md-5 vh-100">
+    <div class="container">
+      <div class="row justify-content-md-center">
+        <div class="col-12 col-md-11 col-lg-8 col-xl-7 col-xxl-6">
+          <div class="bg-white p-4 p-md-5 rounded shadow-sm">
+            <h2 class="h3 mb-4">會員註冊</h2>
+            <form @submit.prevent="">
+              <div class="form-floating mb-4">
+                <input
+                  type="text"
+                  class="form-control"
+                  id="firstName"
+                  placeholder="王小明"
+                  required
+                />
+                <label for="firstName"
+                  >姓名 <span class="text-danger">*</span></label
+                >
+              </div>
+
+              <div class="form-floating mb-4">
+                <input
+                  type="email"
+                  class="form-control"
+                  id="email"
+                  placeholder="example@gmail.com"
+                  pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+                  required
+                />
+                <label for="email"
+                  >信箱 <span class="text-danger">*</span></label
+                >
+              </div>
+
+              <div class="form-floating mb-4">
+                <input
+                  type="password"
+                  class="form-control"
+                  id="password"
+                  placeholder="請輸入 8 碼以上密碼"
+                  pattern=".{8,}"
+                  required
+                />
+                <label for="password"
+                  >密碼 <span class="text-danger">*</span></label
+                >
+              </div>
+
+              <div class="form-floating mb-4">
+                <input
+                  type="tel"
+                  class="form-control"
+                  id="phone"
+                  placeholder="0912345678"
+                  pattern="(\+886|0)?9\d{8}|(\+886|0)?2\d{8}|\d{3}-\d{4}-\d{4}"
+                  required
+                />
+                <label for="phone">電話</label>
+              </div>
+
+              <div class="form-floating mb-4">
+                <input
+                  type="date"
+                  class="form-control"
+                  id="dateInput"
+                  required
+                />
+                <label for="dateInput">出生年月日</label>
+              </div>
+
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="form-floating mb-4">
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="zipcode"
+                      placeholder="100"
+                      pattern="\d{3,}"
+                      required
+                    />
+                    <label for="zipcode">郵遞區號</label>
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="form-floating mb-4">
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="address"
+                      placeholder="台北市中正區重慶南路一段"
+                      required
+                    />
+                    <label for="address">詳細地址</label>
+                  </div>
+                </div>
+              </div>
+
+              <button class="btn btn-lg btn-primary w-100" type="submit">
+                註冊
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
