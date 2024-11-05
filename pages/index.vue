2ade984e3e1a846976f3800b4f158d74ea8cb5a4
Steven Zhong <89566389+y7516552@users.noreply.github.com>
2024-11-05 15:32:45 +0800

    setup

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,22 @@
 
 <template>
   <div>
-    Page: 首頁
+    <h1>Page: 首頁</h1>
+    <button type="button" class="btn btn-primary">Primary</button>
+    <button type="button" class="btn btn-secondary">Secondary</button>
+    <button type="button" class="btn btn-success">Success</button>
+    <button type="button" class="btn btn-danger">Danger</button>
+    <button type="button" class="btn btn-warning">Warning</button>
+    <button type="button" class="btn btn-info">Info</button>
+    <button type="button" class="btn btn-light">Light</button>
+    <button type="button" class="btn btn-dark">Dark</button>
+    <button type="button" class="btn btn-link">Link</button>
   </div>
 </template>
 
-<style scoped></style>
+<style lang="scss">
+h1{
+  color: $primary;
+  font-size: $fs-xl;
+}
+</style>
