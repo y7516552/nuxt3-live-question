64483fc66c329dc809179efa1845a0e4161a7aed
Steven Zhong <89566389+y7516552@users.noreply.github.com>
2024-11-18 17:19:20 +0800

    add head meta tag

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,51 @@
-<script setup></script>
+<script setup>
+useHead({
+  title: "Freyja | 房型列表",
+  meta: [
+    {
+      name: "description",
+      content: "探索 Freyja 頂級房型，從景觀尊榮家庭房到尊爵雙人房，享受絕美市景與舒適空間。立即預訂，享受獨特的住宿體驗！",
+    },
+    {
+      property: "og:title",
+      content: "Freyja | 高雄最頂級的旅館",
+    },
+    {
+      property: "og:description",
+      content: "探索 Freyja 的高雄頂級房型，從景觀尊榮家庭房到尊爵雙人房，享受絕美市景與舒適空間。立即預訂，享受獨特的住宿體驗！",
+    },
+    { 
+      property: "og:image", 
+      content: "https://raw.githubusercontent.com/hexschool/2022-web-layout-training/main/typescript-hotel/%E6%A1%8C%E6%A9%9F%E7%89%88/room2-1.png" 
+    },
+    {
+      property: "og:url",
+      content: "https://freyja.travel.com.tw/room",
+    },
+
+    {
+      property: "twitter:card",
+      content: "summary_large_image",
+    },
+    {
+      property: "twitter:title",
+      content: "Freyja | 高雄最頂級的旅館",
+    },
+    {
+      property: "twitter:description",
+      content: "探索 Freyja 的高雄頂級房型，從景觀尊榮家庭房到尊爵雙人房，享受絕美市景與舒適空間。立即預訂，享受獨特的住宿體驗！",
+    },
+    { 
+      property: "twitter:image", 
+      content: "https://raw.githubusercontent.com/hexschool/2022-web-layout-training/main/typescript-hotel/%E6%A1%8C%E6%A9%9F%E7%89%88/room2-1.png" 
+    },
+    
+    
+  ],
+});
+
+
+</script>
 
 <template>
   <h1>首頁頁面</h1>
