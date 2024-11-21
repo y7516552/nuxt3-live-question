0a426cf5dd4617b6ec66a8a33138ce05111fc169
Steven Zhong <89566389+y7516552@users.noreply.github.com>
2024-11-21 10:42:19 +0800

    add login function & useCookie

--- pages/login.vue
+++ pages/login.vue
@@ -7,7 +7,42 @@ const userLoginObject = ref({
   password: "",
 });
 
-const loginAccount = () => {
+const loginAccount =  async (userData) => {
+  try{
+    const res = await $fetch("/v1/user/login", {
+      baseURL: "https://nuxr3.zeabur.app/api",
+      method: "POST",
+      body: {
+        ...userData,
+      },
+    });
+
+
+    const { token , result} = res
+
+
+    const cookie = useCookie("auth", {
+        path: "/",
+      });
+      cookie.value = token;    
+    await $swal.fire({
+      position: "center",
+      icon: "success",
+      title: `登入成功, ${result.name}`,
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  }catch( error ) {
+    const { message } = error.response._data;
+    $swal.fire({
+      position: "center",
+      icon: "error",
+      title: message,
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  }
+
   /*
   1. 串接旅館的 登入 API
   2. 登入成功後，使用 useCookie() 將 token 寫入名稱為 “auth” 的 cookie
@@ -30,9 +65,10 @@ const loginAccount = () => {
       <div class="row justify-content-md-center">
         <div class="col-12 col-md-11 col-lg-8 col-xl-7 col-xxl-6">
           <h2 class="h3 mb-4">登入</h2>
-          <form>
+          <form @submit.prevent="loginAccount(userLoginObject)">
             <div class="form-floating mb-4">
               <input
+                v-model="userLoginObject.email"
                 type="email"
                 class="form-control"
                 id="email"
@@ -45,6 +81,7 @@ const loginAccount = () => {
 
             <div class="form-floating mb-4">
               <input
+                v-model="userLoginObject.password"
                 type="password"
                 class="form-control"
                 id="password"
@@ -56,7 +93,7 @@ const loginAccount = () => {
                 >密碼 <span class="text-danger">*</span></label
               >
             </div>
-            <button class="btn btn-lg btn-primary w-100" type="submit">
+            <button  class="btn btn-lg btn-primary w-100" type="submit">
               登入
             </button>
           </form>
