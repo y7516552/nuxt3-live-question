6539192fdc2085bbc7a919636cbf47fe9084e093
Steven Zhong <89566389+y7516552@users.noreply.github.com>
2024-11-27 18:03:41 +0800

    add loading.client.js

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,19 @@
 <script setup>
+import {useLoading} from 'vue-loading-overlay'
+
+const $loading = useLoading({
+  transition:"fade",
+  loader:"bars",
+  color:"#fa0",
+  backgroundColor:"#888",
+});
+
 function openLoading() {
   // 開啟讀取效果
+  const loader = $loading.show();
   setTimeout(() => {
     // 關閉讀取效果
+    loader.hide()
   }, 1000);
 }
 </script>
